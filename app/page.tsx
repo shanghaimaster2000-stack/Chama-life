666dcd1aaa96429a5b944a3435e263ab38595104
Yuichi <y.sasai@hardlock.co.jp>
2026-03-15 05:35:11 +0900

    fix onresult event type

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ function analyzeFoodLog(text:string){
 
 useEffect(() => {
   if (!recognition) return;
-  recognition.onresult = (event) => {
+  rrecognition.onresult = (event: any) => {
     let text = "";
     for (let i = 0; i < event.results.length; i++) {
       text += event.results[i][0].transcript;
